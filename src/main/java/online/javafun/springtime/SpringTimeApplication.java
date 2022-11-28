dbd4412699f0e5c6c792122e5905487970dfe79c
User <user@its.me>
2022-11-28 05:05:03 +0100

    Let's Spring happen

---
+++ src/main/java/online/javafun/springtime/SpringTimeApplication.java
@@ -0,0 +1,13 @@
+package online.javafun.springtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTimeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/online/javafun/springtime/SpringTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package online.javafun.springtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTimeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
